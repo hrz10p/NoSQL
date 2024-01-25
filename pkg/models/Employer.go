6cfe3ec1a10c5e152b643f71ff2f343544e0affb
yerlan <yerlankuanysh@gmail.com>
2024-01-25 22:07:05 +0600

    small fix

--- pkg/models/Employer.go
+++ pkg/models/Employer.go
@@ -4,7 +4,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Employer struct {
 	ID          primitive.ObjectID `bson:"_id"`
-	Username    string             `bson:"username"`
+	Username    string             `bson:"name"`
 	Password    string             `bson:"password"`
 	CompanyName string             `bson:"company_name"`
 	Vacancies   []Vacancy          `bson:"vacancies"`
